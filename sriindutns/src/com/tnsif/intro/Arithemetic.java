b6d427a68a018711fe3736a2f8ae4d896dbb6e99
ksrao <ksrao@DESKTOP-VL41FA8>
2025-12-23 17:03:44 +0530

    arithemetic

---
+++ sriindutns/src/com/tnsif/intro/Arithemetic.java
@@ -0,0 +1,21 @@
+package com.tnsif.intro;
+
+public class Arithemetic {
+	public static void main(String [] args)
+	{
+		int a=10;
+		int b=20;
+		int r;
+		r=a+b;
+		System.out.println(r);
+		r=a-b;
+		System.out.println(r);
+		r=a*b;
+		System.out.println(r);
+		r=a/b;
+		System.out.println(r);
+		r=a%b;
+		System.out.println(r);
+	}
+	
+}
