4593756ec17c9659b0bef5b4788f2fee75d7958b
ksrao <ksrao@DESKTOP-VL41FA8>
2025-12-31 22:58:36 +0530

    Do while

---
+++ sriindutns/src/com/tnsif/intro/loopdw.java
@@ -0,0 +1,12 @@
+package com.tnsif.intro;
+
+public class loopdw {
+	public static void main(String[] args) {
+		
+		int p = 30;
+		do {
+			System.out.println(p);
+			p++;
+		} while (p<=60);
+	}
+}
