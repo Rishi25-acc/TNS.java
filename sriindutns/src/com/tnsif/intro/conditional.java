b32f5ccce5cc5e643cb3b1e7e1dabb9e1437e543
ksrao <ksrao@DESKTOP-VL41FA8>
2026-01-01 21:26:24 +0530

    Conditional

---
+++ sriindutns/src/com/tnsif/intro/conditional.java
@@ -0,0 +1,17 @@
+package com.tnsif.intro;
+
+public class conditional {
+	public static void main(String[] args)
+	{
+		int a = 20;
+		int b = 30;
+		if(a>b)
+		{
+			System.out.println("if");
+		}
+		else if (a<b)
+		{
+			System.out.println("else");
+		}
+    }
+}
