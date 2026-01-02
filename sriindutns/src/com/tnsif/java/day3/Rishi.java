ac27a32f64dfb6b9bea0963bbefaabf96c07c331
ksrao <ksrao@DESKTOP-VL41FA8>
2026-01-02 19:42:58 +0530

    Calling A String by an object

---
+++ sriindutns/src/com/tnsif/java/day3/Rishi.java
@@ -0,0 +1,15 @@
+package com.tnsif.java.day3;
+
+public class Rishi {
+	String mname = "Pooja";
+	int mid=37;
+	
+	Rishi()
+	{
+		System.out.println("RISHI NAME:"+mname);
+	}
+	Rishi(int a)
+	{
+		System.out.println("RISHI ID:"+mid+"The value of a:"+a);
+		}
+	}
---
+++ sriindutns/src/com/tnsif/java/day3/Rishidemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.day3;
+
+public class Rishidemo {
+	
+	public static void main(String[] args) {
+		Rishi r = new Rishi();
+		Rishi r1 = new Rishi(37);
+		
+	}
+
+}
