32fcdfecd92ffd60ea54baff269226f92a2e19c5
ksrao <ksrao@DESKTOP-VL41FA8>
2026-01-04 16:40:35 +0530

    KEYWORD - Final

---
+++ sriindutns/src/com/tnsif/java/day5/Final.java
@@ -0,0 +1,9 @@
+package com.tnsif.java.day5;
+
+public class Final {
+	int a = 30;
+	public void show() {
+		System.out.println("Main method");
+	}
+
+}
---
+++ sriindutns/src/com/tnsif/java/day5/Finaldemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.day5;
+
+public class Finaldemo 
+{
+	public static void main(String[] args)
+	{
+		Final f = new Final();
+		f.show();
+	}
+
+}
