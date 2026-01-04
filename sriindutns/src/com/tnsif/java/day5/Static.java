109691a2080bc7ff1e93aeb093a69142b94d69bf
ksrao <ksrao@DESKTOP-VL41FA8>
2026-01-04 15:43:41 +0530

    KEYWORD-Static

---
+++ sriindutns/src/com/tnsif/java/day5/Static.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.day5;
+
+public class Static {
+	static int a = 30;
+	static public void show() {
+		
+	}
+    static {
+    	System.out.println("static method");
+    }
+}
---
+++ sriindutns/src/com/tnsif/java/day5/Staticdemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.java.day5;
+
+public class Staticdemo {
+	public static void main(String[] args) {
+		System.out.println("static.a:");
+		Static.show();
+	}
+
+}
