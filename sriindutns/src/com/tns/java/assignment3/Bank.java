d4a1d0a8197298fc285337baf8043709423fe995
ksrao <ksrao@DESKTOP-VL41FA8>
2026-01-31 20:17:21 +0530

    Assignment - BANK

---
+++ sriindutns/src/com/tns/java/assignment3/Bank.java
@@ -0,0 +1,10 @@
+package com.tns.java.assignment3;
+
+public interface Bank {
+	int MIN_BALANCE=1000;
+	int DEPOSIT_LIMIT=10000;
+	void deposit();
+	
+	void withdraw();
+
+}
---
+++ sriindutns/src/com/tns/java/assignment3/BankImplentation.java
@@ -0,0 +1,8 @@
+package com.tns.java.assignment3;
+
+public abstract class BankImplentation  implements Bank {
+	public abstract void deposit();
+	
+	public abstract void withdraw();
+
+}
